c47ff31837cc596faf81faab85a0a3937ae72cbb
Marcin Krysiński <marcin_krysinski@outlook.com>
2021-04-02 16:29:50 +0200

    FRDQ-241 - change schema validation

--- backend/validators/user.validator.ts
+++ backend/validators/user.validator.ts
@@ -5,21 +5,15 @@ const schema = {
   post: Joi.object({
     name: Joi.string().min(4).required(),
     title: Joi.string().required(),
-    openToWork: Joi.boolean().default(true),
-    manager: Joi.boolean().default(false),
     boss: Joi.string().default(null),
     image: Joi.string().allow(""),
     socials: Joi.array().items(socialSchema.post).allow().empty(),
-    active: Joi.boolean().default(true)
   }),
   put: Joi.object({
     name: Joi.string().min(4),
     title: Joi.string(),
-    openToWork: Joi.boolean().default(true),
-    manager: Joi.boolean().default(false),
     boss: Joi.string().default(null),
     image: Joi.string().allow(""),
-    active: Joi.boolean().default(true)
   }),
   id: Joi.object().keys({
     id: Joi.string().length(24)
