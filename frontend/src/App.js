12a8ec29396678d9c075fb19f1f2bb44bbbcf9c2
Hayuna <marek.kowalonek@gmail.com>
2021-04-10 15:57:29 +0200

    typofix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Structure from './pages/structure'
+import Structure from './pages/Structure'
 
 function App() {
   return (
