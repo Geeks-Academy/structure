bd4b77bfcd3b190c482832799c924eb8873ae63f
Marcin Krysiński <marcin_krysinski@outlook.com>
2021-04-11 15:52:13 +0200

    FRDQ-259 - move instance of express app to separate file

--- backend/app.ts
+++ backend/app.ts
@@ -5,55 +5,94 @@ import routes from "./routes";
 import connectDb from "./services/database.service";
 import cors from 'cors'
 
-
-const main = async () => {
-  await connectDb();
-  const app = express();
-
-  app.use(express.json());
-
-  app.use(
-    cors({
-      credentials: true,
-      origin:
-        process.env.NODE_ENV === "production"
-          ? process.env.PROD_URL
-          : process.env.DEV_URL,
-    })
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.PROD_URL
+        : process.env.DEV_URL,
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
+  next();
+});
 
-  app.use(express.json());
+app.use("/api", routes);
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Credentials");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+app.get("/health-check", (_req, res) => {
+  res.status(200).json({ ok: true, message: "Health check" });
+});
 
-  app.get("/health-check", (_req, res) => {
-    res.status(200).json({ ok: true, message: "Health check" });
-  });
+app.use((_req, res, _next) => {
+  res.status(404).json({ message: "Not found" });
+});
 
-  app.use("/api", routes);
 
-  app.use((_req, res, _next) => {
-    res.status(404).json({ message: "Not found" });
-  });
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
 
-  app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "Internal server error" });
-  });
+export default app;
+// const main = async () => {
+//   await connectDb();
+//   const app = express();
 
-  const PORT = +(process.env.port as string) || 4000;
+//   app.use(express.json());
 
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
-};
+//   app.use(
+//     cors({
+//       credentials: true,
+//       origin:
+//         process.env.NODE_ENV === "production"
+//           ? process.env.PROD_URL
+//           : process.env.DEV_URL,
+//     })
+//   );
 
-main();
+//   app.use(express.json());
+
+//   app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", req.headers.origin);
+//     res.header("Access-Control-Allow-Credentials");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
+
+//   app.get("/health-check", (_req, res) => {
+//     res.status(200).json({ ok: true, message: "Health check" });
+//   });
+
+//   app.use("/api", routes);
+
+//   app.use((_req, res, _next) => {
+//     res.status(404).json({ message: "Not found" });
+//   });
+
+//   app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+//     console.error(err.stack);
+//     res.status(500).json({ message: "Internal server error" });
+//   });
+
+//   const PORT = +(process.env.port as string) || 4000;
+
+//   app.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`);
+//   });
+// };
+
+
+
+// main();
---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+import connectDb from "./services/database.service";
+import app from './app';
+
+const main = async () => {
+  await connectDb();
+
+  const PORT = +(process.env.port as string) || 4000;
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+
+main();
+
