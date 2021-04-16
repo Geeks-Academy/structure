945077644372116ec9cd777e232bae067ab4092e
Hayuna <marek.kowalonek@gmail.com>
2021-04-17 00:30:03 +0200

    FRDQ-216 - Fix styles

--- frontend/src/styles/GlobalStyle.tsx
+++ frontend/src/styles/GlobalStyle.tsx
@@ -1,48 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-  *, 
-  *::before, 
-  *::after {  
-    box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    margin:0;
-    padding:0;
-    outline:none;
-  }
-
-
-  button {
-    border: none;
-    background: none;
-    cursor: pointer;  
-
-  }  
-
-  html {
-    font-size: 62.5%; 
-  }
-  
-  ul {
-    list-style: none; 
-  }
-
-  body {
-    width:100%;
-    height:100vh;
-    font-size: 1.6rem;
-    font-family: "Roboto", sans-serif;
-    overflow-x:hidden;
-    position:relative;
-    margin:0;
-    padding:0;
-    outline:none;
-  }
-
-  a {
-    text-decoration: none;
-  }
-`;
+const GlobalStyle = createGlobalStyle``;
 
 export default GlobalStyle;
