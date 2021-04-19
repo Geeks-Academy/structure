9589f326fae335fed36dfe24b6f0d7a63a406ce8
Hayuna <marek.kowalonek@gmail.com>
2021-04-19 17:18:17 +0200

    FRDQ-233 - Remove unnecessary file

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,11 +1,13 @@
-import Structure from "pages/Structure";
-import { Route, Switch } from "react-router-dom";
+import Structure from 'pages/Structure';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 const Routes = (): JSX.Element => {
   return (
-    <Switch>
-      <Route exact path="/" component={Structure} />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Structure} />
+      </Switch>
+    </Router>
   );
 };
 
