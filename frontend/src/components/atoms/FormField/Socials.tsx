d39d4016eabaa2784c93c894e686658fd8b1e67f
Konrad Rudnicki <zeglarz@gmail.com>
2021-06-05 20:26:44 +0200

    FRDQ-106 present img and field values

--- frontend/src/components/atoms/FormField/Socials.tsx
+++ frontend/src/components/atoms/FormField/Socials.tsx
@@ -1,10 +1,13 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useState } from 'react';
 import { useController, FieldError, Control } from 'react-hook-form';
 import ErrorMessage from 'components/atoms/ErrorMessage';
 import { StyledSelectWrapper, StyledOption, StyledSelect, StyledLabel } from './Select.styled';
 import { IUser } from '../../../Types/interfaces';
 import { StyledInput } from './Input.styled';
+import { useAsyncEffect } from '../../../hooks';
+import { removeCurrentUser, replaceUserInfoIntoSelectOptions } from '../../../helpers';
+import { SocialRequests } from '../../../Services/Requests/socials';
 
 interface IProps {
   name: string;
@@ -15,7 +18,7 @@ interface IProps {
 
 const Socials = ({ control, name, label, error }: IProps): JSX.Element => {
   const {
-    field: { ref, ...inputProps },
+    field: { ref, value, ...inputProps },
   } = useController({
     name,
     control,
@@ -24,14 +27,19 @@ const Socials = ({ control, name, label, error }: IProps): JSX.Element => {
   return (
     <StyledSelectWrapper>
       <StyledLabel>{label}</StyledLabel>
-      <StyledInput {...inputProps} inputRef={ref}>
-        {control.defaultValuesRef.current.socials.map(social => {
-          console.log(control.defaultValuesRef.current.socials);
+      <div>
+        {console.log(value)}
+      {value.map(social => {
+        console.log(social.social.name);
 
-          console.log(social);
-          return <img/>
-        })}
-      </StyledInput>
+        return <img src={social.social.image} onClick={() => console.log(social.social.image)}/>
+      })}
+      </div>
+      {value.map((val, idx) => {
+        console.log(val.link);
+        return (<StyledInput value={val.link} {...inputProps[idx]} inputRef={ref[idx]}>
+        </StyledInput>)
+      })}
       {error && <ErrorMessage>{error.message}</ErrorMessage>}
     </StyledSelectWrapper>
   );
