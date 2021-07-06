2475c345e35a6c17288d72fe35f9b76b0ae457b1
Hayuna <marek.kowalonek@gmail.com>
2021-07-06 17:11:14 +0200

    FRDQ-106 - Test

--- backend/__tests__/setup.ts
+++ backend/__tests__/setup.ts
@@ -4,33 +4,49 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 const mongod = new MongoMemoryServer();
 
 beforeAll(async () => {
-  console.log = () => {};
-  const uri = await mongod.getUri();
-  const mongooseOpts = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    poolSize: 10,
-  };
-  await mongoose.connect(uri, mongooseOpts);
+  try {
+    console.log = () => {};
+    const uri = await mongod.getUri();
+    const mongooseOpts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      poolSize: 10,
+    };
+    await mongoose.connect(uri, mongooseOpts);
+  } catch (e) {
+    console.info(e);
+  }
 });
 
 beforeEach(async () => {
-  const collections = mongoose.connection.collections;
-  for (const key in collections) {
-    const collection = collections[key];
-    await collection.deleteMany({});
+  try {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany({});
+    }
+  } catch (e) {
+    console.info(e);
   }
 });
 
 afterAll(async () => {
-  await mongoose.connection.close();
-  await mongod.stop();
+  try {
+    await mongoose.connection.close();
+    await mongod.stop();
+  } catch (e) {
+    console.info(e);
+  }
 });
 
 afterEach(async () => {
-  jest.restoreAllMocks();
-  console.info(mongoose.connection);
-  await mongoose.connection.dropDatabase();
+  try {
+    jest.restoreAllMocks();
+    console.info(mongoose.connection);
+    await mongoose.connection.dropDatabase();
+  } catch (e) {
+    console.info(e);
+  }
 });
