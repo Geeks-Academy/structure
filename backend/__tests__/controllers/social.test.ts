67c5da65210f602a5527578de6858c74c3a43e98
Hayuna <marek.kowalonek@gmail.com>
2021-07-06 17:48:40 +0200

    FRDQ-106 - Test

--- backend/__tests__/controllers/social.test.ts
+++ backend/__tests__/controllers/social.test.ts
@@ -1,7 +1,7 @@
 import app from '../../src/app';
 import supertest from 'supertest';
 
-describe('Social Controller', () => {
+describe('Social Controller', async () => {
   test('GET /api/socials empty socials', async () => {
     await supertest(app).get('/api/socials').expect(204);
   });
