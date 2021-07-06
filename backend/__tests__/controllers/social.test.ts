a9845226b69725a8f67cabe82fbb3963efd17f54
Hayuna <marek.kowalonek@gmail.com>
2021-07-06 17:42:00 +0200

    FRDQ-106 - Test

--- backend/__tests__/controllers/social.test.ts
+++ backend/__tests__/controllers/social.test.ts
@@ -1,42 +1,8 @@
 import app from '../../src/app';
 import supertest from 'supertest';
-import Social from '../../src/models/social.model';
 
 describe('Social Controller', () => {
   test('GET /api/socials empty socials', async () => {
     await supertest(app).get('/api/socials').expect(204);
-  });
-
-  test('GET /api/socials should throw exception', async () => {
-    jest.spyOn(Social, 'find').mockImplementation(() => {
-      throw new Error('Error');
-    });
-    await supertest(app)
-      .get('/api/socials')
-      .expect(500)
-      .then(({ body }) => {
-        expect(body).toHaveProperty('message');
-      });
-  });
-
-  test('GET /api/socials', async (done) => {
-    const social = await Social.create({
-      name: 'Facebook',
-      image: 'fb.png',
-    });
-    await supertest(app)
-      .get('/api/socials')
-      .expect(200)
-      .then(({ body }) => {
-        expect(Array.isArray(body)).toBeTruthy();
-        expect(body.length).toEqual(1);
-        expect(body[0]._id).toBe(social.id);
-        expect(body[0].name).toBe(social.name);
-      })
-      .finally(() => done());
-  });
-
-  test('GET /api/socials/active empty socials', async () => {
-    await supertest(app).get('/api/socials/active').expect(204);
   });
 });
