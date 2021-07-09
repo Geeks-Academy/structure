0e9b873fb90d84390d226af3f7850e9623014541
Hayuna <marek.kowalonek@gmail.com>
2021-07-09 18:10:59 +0200

    FRDQ-1000 - Cleanup

--- backend/src/controllers/cloudinary.controller.ts
+++ backend/src/controllers/cloudinary.controller.ts
@@ -2,16 +2,16 @@ import { Request, Response } from 'express';
 import { detectImage, cloudinaryUpload, deleteImage } from '../services/cloudinary';
 
 export const upload = async (req: Request, res: Response): Promise<Response> => {
-    const image = req.body.image;
-    const uploadResult: any = await cloudinaryUpload(image, {
-      upload_preset: process.env.UPLOAD_PRESET,
-    });
+  const image = req.body.image;
+  const uploadResult: any = await cloudinaryUpload(image, {
+    upload_preset: process.env.UPLOAD_PRESET,
+  });
 
-    const data = await detectImage(uploadResult);
+  const data = await detectImage(uploadResult);
 
-    if (!data.url) {
-      deleteImage(uploadResult.public_id);
-    }
+  if (!data.url) {
+    deleteImage(uploadResult.public_id);
+  }
 
-    return res.json({ data });
+  return res.json({ data });
 };
