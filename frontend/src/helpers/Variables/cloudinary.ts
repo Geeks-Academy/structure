e19dd2cfe1932869a5f8c2c7c929dcd835c5e0b2
Hayuna <marek.kowalonek@gmail.com>
2021-07-31 23:30:48 +0200

    FRDQ-1000 - Update placeholder image

--- frontend/src/helpers/Variables/cloudinary.ts
+++ frontend/src/helpers/Variables/cloudinary.ts
@@ -1,2 +1,2 @@
 export const userPlaceholder =
-  'https://res.cloudinary.com/dokasambz/image/upload/v1623694068/user_images/user_placeholder_aglohv.png';
+  'https://res.cloudinary.com/mras2303/image/upload/v1621967782/members/T017TS5J06T-U01AKNF7G9X-gfd207e3c159-512.png';
